43c0620b8cfed5674e5c453f3e71cd81022b5033
deviroh <dev.sriram.pers@gmail.com>
2026-07-07 21:07:40 +0530

    Console Commit

--- App.js
+++ App.js
@@ -1,30 +1,21 @@
-import { useState } from 'react';
-import {
-  View,
-  StyleSheet,
-  Platform,
-  StatusBar,
-  Text,
-} from 'react-native';
-import Focus from './src/features/Focus';
-import Timer from './src/components/Timer';
+import { useState } from "react";
+import { View, StyleSheet, Platform, StatusBar, Text } from "react-native";
+import Focus from "./src/features/Focus";
+import Timer from "./src/components/Timer";
 
 export default function App() {
-  const [currentSubject, setCurrentSubject] = useState('');
-  return (
-    <View style={styles.container}>
-      {!currentSubject ? (
-         <Focus addSubject={setCurrentSubject} />
-      ): (
-        <Timer focusSubject={currentSubject} /> 
-      )}
-    </View>
-  );
+    const [currentSubject, setCurrentSubject] = useState("");
+    console.log(currentSubject);
+    return (
+        <View style={styles.container}>
+            {!currentSubject ? <Focus addSubject={setCurrentSubject} /> : <Timer focusSubject={currentSubject} />}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
+    container: {
+        flex: 1,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
 });
